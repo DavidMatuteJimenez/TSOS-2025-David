6d7fc90b5939a1344689050d4e3e4198e035abfd
DavidMatuteJimenez <david.matutejimenez1@marist.edu>
2025-09-18 16:06:35 -0400

    back to original code

--- source/os/shell.ts
+++ source/os/shell.ts
@@ -1,13 +1,17 @@
 /* ------------
-   Shell.ts
+  Shell.ts
 
-   The OS Shell - The "command line interface" (CLI) for the console.
 
-    Note: While fun and learning are the primary goals of all enrichment center activities,
-          serious injuries may occur when trying to write your own Operating System.
-   ------------ */
+  The OS Shell - The "command line interface" (CLI) for the console.
+
+
+   Note: While fun and learning are the primary goals of all enrichment center activities,
+         serious injuries may occur when trying to write your own Operating System.
+  ------------ */
+
 
 // TODO: Write a base class / prototype for system services and let Shell inherit from it.
+
 
 module TSOS {
     export class Shell {
@@ -16,89 +20,88 @@ module TSOS {
         public commandList = [];
         public curses = "[fuvg],[cvff],[shpx],[phag],[pbpxfhpxre],[zbgureshpxre],[gvgf]";
         public apologies = "[sorry]";
-
+ 
+ 
         constructor() {
         }
+ 
+ 
         public init() {
             var sc: ShellCommand;
             //
             // Load the command list.
-
+ 
+ 
             // ver
             sc = new ShellCommand(this.shellVer,
                                   "ver",
                                   "- Displays the current version data.");
             this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // help
             sc = new ShellCommand(this.shellHelp,
                                   "help",
                                   "- This is the help command. Seek help.");
             this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // shutdown
             sc = new ShellCommand(this.shellShutdown,
                                   "shutdown",
                                   "- Shuts down the virtual OS but leaves the underlying host / hardware simulation running.");
             this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // cls
             sc = new ShellCommand(this.shellCls,
                                   "cls",
                                   "- Clears the screen and resets the cursor position.");
             this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // man <topic>
             sc = new ShellCommand(this.shellMan,
                                   "man",
                                   "<topic> - Displays the MANual page for <topic>.");
             this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // trace <on | off>
             sc = new ShellCommand(this.shellTrace,
                                   "trace",
                                   "<on | off> - Turns the OS trace on or off.");
             this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // rot13 <string>
             sc = new ShellCommand(this.shellRot13,
                                   "rot13",
                                   "<string> - Does rot13 obfuscation on <string>.");
             this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // prompt <string>
             sc = new ShellCommand(this.shellPrompt,
                                   "prompt",
                                   "<string> - Sets the prompt.");
             this.commandList[this.commandList.length] = sc;
-
-            // status <string>
-            sc = new ShellCommand(this.shellStatus,
-                                  "status",
-                                  "<string> - Sets the status message displayed in the taskbar.");
-            this.commandList[this.commandList.length] = sc;
-
+ 
+ 
             // ps  - list the running processes and their IDs
             // kill <id> - kills the specified process id.
-
+ 
+ 
             // Display the initial prompt.
             this.putPrompt();
         }
-
-        public shellStatus(args: string[]) {
-            if (args.length > 0) {
-                var statusMessage = args.join(' ');
-                Control.updateTaskbarStatus(statusMessage);
-                _StdOut.putText("Status set to: " + statusMessage);
-            } else {
-                _StdOut.putText("Usage: status <string>  Please supply a status message.");
-            }
-        }
-
+ 
+ 
         public putPrompt() {
             _StdOut.putText(this.promptStr);
         }
-
+ 
+ 
         public handleInput(buffer) {
             _Kernel.krnTrace("Shell Command~" + buffer);
             //
@@ -112,7 +115,7 @@ module TSOS {
             // Determine the command and execute it.
             //
             // TypeScript/JavaScript may not support associative arrays in all browsers so we have to iterate over the
-            // command list in attempt to find a match. 
+            // command list in attempt to find a match.
             // TODO: Is there a better way? Probably. Someone work it out and tell me in class.
             var index: number = 0;
             var found: boolean = false;
@@ -138,7 +141,8 @@ module TSOS {
                 }
             }
         }
-
+ 
+ 
         // Note: args is an optional parameter, ergo the ? which allows TypeScript to understand that.
         public execute(fn, args?) {
             // We just got a command, so advance the line...
@@ -152,26 +156,32 @@ module TSOS {
             // ... and finally write the prompt again.
             this.putPrompt();
         }
-
+ 
+ 
         public parseInput(buffer: string): UserCommand {
             var retVal = new UserCommand();
-
+ 
+ 
             // 1. Remove leading and trailing spaces.
             buffer = Utils.trim(buffer);
-
+ 
+ 
             // 2. Lower-case it.
             buffer = buffer.toLowerCase();
-
+ 
+ 
             // 3. Separate on spaces so we can determine the command and command-line args, if any.
             var tempList = buffer.split(" ");
-
+ 
+ 
             // 4. Take the first (zeroth) element and use that as the command.
             var cmd = tempList.shift();  // Yes, you can do that to an array in JavaScript. See the Queue class.
             // 4.1 Remove any left-over spaces.
             cmd = Utils.trim(cmd);
             // 4.2 Record it in the return value.
             retVal.command = cmd;
-
+ 
+ 
             // 5. Now create the args array from what's left.
             for (var i in tempList) {
                 var arg = Utils.trim(tempList[i]);
@@ -181,7 +191,8 @@ module TSOS {
             }
             return retVal;
         }
-
+ 
+ 
         //
         // Shell Command Functions. Kinda not part of Shell() class exactly, but
         // called from here, so kept here to avoid violating the law of least astonishment.
@@ -196,14 +207,16 @@ module TSOS {
                 _StdOut.putText("Type 'help' for, well... help.");
             }
         }
-
+ 
+ 
         public shellCurse() {
             _StdOut.putText("Oh, so that's how it's going to be, eh? Fine.");
             _StdOut.advanceLine();
             _StdOut.putText("Bitch.");
             _SarcasticMode = true;
         }
-
+ 
+ 
         public shellApology() {
            if (_SarcasticMode) {
               _StdOut.putText("I think we can put our differences behind us.");
@@ -214,14 +227,17 @@ module TSOS {
               _StdOut.putText("For what?");
            }
         }
-
-        // Although args is unused in some of these functions, it is always provided in the 
+ 
+ 
+        // Although args is unused in some of these functions, it is always provided in the
         // actual parameter list when this function is called, so I feel like we need it.
-
+ 
+ 
         public shellVer(args: string[]) {
             _StdOut.putText(APP_NAME + " version " + APP_VERSION);
         }
-
+ 
+ 
         public shellHelp(args: string[]) {
             _StdOut.putText("Commands:");
             for (var i in _OsShell.commandList) {
@@ -229,19 +245,22 @@ module TSOS {
                 _StdOut.putText("  " + _OsShell.commandList[i].command + " " + _OsShell.commandList[i].description);
             }
         }
-
+ 
+ 
         public shellShutdown(args: string[]) {
              _StdOut.putText("Shutting down...");
              // Call Kernel shutdown routine.
             _Kernel.krnShutdown();
             // TODO: Stop the final prompt from being displayed. If possible. Not a high priority. (Damn OCD!)
         }
-
-        public shellCls(args: string[]) {         
-            _StdOut.clearScreen();     
+ 
+ 
+        public shellCls(args: string[]) {        
+            _StdOut.clearScreen();    
             _StdOut.resetXY();
         }
-
+ 
+ 
         public shellMan(args: string[]) {
             if (args.length > 0) {
                 var topic = args[0];
@@ -267,9 +286,6 @@ module TSOS {
                     case "prompt":
                         _StdOut.putText("prompt - sets the prompt.");
                         break;
-                    case "status":
-                        _StdOut.putText("status - Sets the status message displayed in the taskbar.");
-                        break;
                     // TODO: Make descriptive MANual page entries for the the rest of the shell commands here.
                     default:
                         _StdOut.putText("No manual entry for " + args[0] + ".");
@@ -278,7 +294,8 @@ module TSOS {
                 _StdOut.putText("Usage: man <topic>  Please supply a topic.");
             }
         }
-
+ 
+ 
         public shellTrace(args: string[]) {
             if (args.length > 0) {
                 var setting = args[0];
@@ -302,7 +319,8 @@ module TSOS {
                 _StdOut.putText("Usage: trace <on | off>");
             }
         }
-
+ 
+ 
         public shellRot13(args: string[]) {
             if (args.length > 0) {
                 // Requires Utils.ts for rot13() function.
@@ -311,7 +329,8 @@ module TSOS {
                 _StdOut.putText("Usage: rot13 <string>  Please supply a string.");
             }
         }
-
+ 
+ 
         public shellPrompt(args: string[]) {
             if (args.length > 0) {
                 _OsShell.promptStr = args[0];
@@ -319,6 +338,8 @@ module TSOS {
                 _StdOut.putText("Usage: prompt <string>  Please supply a string.");
             }
         }
-
+ 
+ 
     }
-}
+ }
+ 
